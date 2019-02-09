59362a39b846a14288a13c80002acd707626f222
Shaalinii <47456505+Shaalinii@users.noreply.github.com>
2019-02-09 16:17:35 +0530

    Create positive negative.c

---
+++ positive negative.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) 
+
+{
+   
+int a,b,c;
+   
+scanf("%d",&a);
+   
+if(a>0)
+  
+ {
+      
+ printf("Positive");
+  
+ }
+  
+ else if(a==0)
+  
+ {
+      
+ printf("Zero");
+  
+  }
+    
+else
+    
+printf("Negative");
+	
+return 0;
+
+}
+
+
+
+
